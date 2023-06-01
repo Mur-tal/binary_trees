6d369e0900515cf2c80eb5ea89a7af542f901df7
Mur-tal <muritala.gbadamosi@yahoo.com>
2023-06-01 08:56:04 -0700

    measures the balance factor of a binary tree

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -32,15 +32,13 @@ size_t binary_tree_height(const binary_tree_t *tree)
 	size_t l = 0, r = 0;
 
 	if (!tree)
+	{
 		return (0);
-
-	if (tree->left)
-		l += 1 + binary_tree_height(tree->left);
-	if (tree->right)
-		r += 1 + binary_tree_height(tree->right);
+	}
+	l += 1 + binary_tree_height(tree->left);
+	r += 1 + binary_tree_height(tree->right);
 
 	if (l > r)
 		return (l);
-	else
-		return (r);
+	return (r);
 }
